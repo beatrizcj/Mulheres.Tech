435951906e1996fe9f86ab722438b585ddee5b66
Beatriz J. <beatrizjesus.senac@gmail.com>
2024-09-20 21:27:08 -0300

    Aula 01 Análise de Dados Big Data 13/09/2024
    Aula onde aprendemos sobre input e float.

---
+++ aula01.py
@@ -0,0 +1,18 @@
+print('STRAY KIDS EVERYWHERE ALL AROUND THE WORLD')     # título do código
+
+'''print('Digite um número:')
+valor = int(input())     # criando uma variável que vai solicitar o usuário a digitar um número, o comando int é para forçar o tipo de dado ser inteiro
+dobro = valor * 2
+print('O valor do dobro é:', dobro)'''
+
+
+print('Digite a nota do primeiro bimestre:')
+B1 = float(input())
+print('Digite a nota do segundo bimestre:')
+B2 = float(input())
+print('Digite a nota do terceiro bimestre:')
+B3 = float(input())
+print('Digite a nota do quarto bimestre:')
+B4 = float(input())
+media = (B1 + B2 + B3 + B4) / 4
+print('A média bimestral é:', media)
